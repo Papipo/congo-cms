9c09f343dce0e5a4a72244a6961914a6833ae084
Papipo <Papipo@PapBook.local>
2009-10-25 19:50:14 +0100

    Updated mongo gems

--- config/environment.rb
+++ config/environment.rb
@@ -14,9 +14,9 @@ Rails::Initializer.run do |config|
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
-  config.gem "mongodb-mongo",     :source => 'http://gems.github.com', :lib => 'mongo'
-  config.gem "mongodb-mongo_ext", :source => 'http://gems.github.com', :lib => false
-  config.gem "mongo_mapper",       :source => 'http://gemcutter.org'
+  config.gem "mongo",     :source => 'http://gemcutter.org'
+  config.gem "mongo_ext", :source => 'http://gemcutter.org', :lib => false
+  config.gem "mongo_mapper",      :source => 'http://gemcutter.org'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
--- config/initializers/mongomapper.rb
+++ config/initializers/mongomapper.rb
@@ -1,3 +1,3 @@
 cfg = Rails.configuration.database_configuration[RAILS_ENV]
-MongoMapper.connection = XGen::Mongo::Driver::Mongo.new(cfg['host'])
+MongoMapper.connection = Mongo::Connection.new(cfg['host'])
 MongoMapper.database   = cfg['database']
